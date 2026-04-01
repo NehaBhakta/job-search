67c51b9550abff170e32697c0494fb779fb90a39
NehaBhakta <154997706+NehaBhakta@users.noreply.github.com>
2026-04-01 21:02:47 +0530

    Update FeaturedDataList.js
    updated

--- src/component/FeaturedDataList.js
+++ src/component/FeaturedDataList.js
@@ -5,13 +5,24 @@ const FeaturedDataList = ({ ListData }) => {
   if (!ListData) return null;
 
   const {
-    title,
+    Titel,
     serviceImg,
     review,
-    company,
-    experience,
-    location,
+    h1,
+    titleone,
+    titletwo,
     time,
+    listone,
+    listtwo,
+    listthree,
+    listfour,
+    listfive,
+    listsix,
+    skilone,
+    skiltwo,
+    skilthree,
+    skilfour,
+    skilfive,
   } = ListData;
 
   return (
@@ -23,11 +34,10 @@ const FeaturedDataList = ({ ListData }) => {
           <div className="feature-header">
             <div className="feature-left-header">
               <div className="feature-img">
-                <img src={serviceImg} alt={title} />
+                <img src={serviceImg} alt={Titel} />
               </div>
-              <h6 className="job-type">{title}</h6>
+              <h6 className="job-type">{Titel}</h6>
             </div>
-
             <button type="button">
               <Link to="">
                 <i className="bi bi-bookmark"></i>
@@ -37,24 +47,77 @@ const FeaturedDataList = ({ ListData }) => {
 
           {/* COMPANY + REVIEW */}
           <div className="review-boxx">
-            <p>{company}</p>
+            <p>{h1}</p>
             <span>
               <i className="bi bi-star-fill"></i> {review}
             </span>
           </div>
 
           {/* DETAILS */}
-          <div className="ex-box">
-            <p>{experience}</p>
-            <p>{location}</p>
-            <p>{time}</p>
+          <div className="common-scrollbar">
+            <div className="ex-box">
+              <div className="ex-wrap">
+                <div className="ex-item">
+                  <div className="w-5">
+                    <i className="bi bi-bag-x"></i>
+                  </div>
+                  <p title="Experience" className="max-w-[64px] w-full truncate">
+                    {titleone}
+                  </p>
+                </div>
+                <div className="ex-item">
+                  <div>
+                    <i className="bi bi-geo-alt"></i>
+                  </div>
+                  <p title="Location" className="max-w-[64px] w-full truncate">
+                    {titletwo}
+                  </p>
+                </div>
+              </div>
+              <p>{time}</p>
+            </div>
+
+            {/* Lists */}
+            <ul className="f-list">
+              {listone && <li>{listone}</li>}
+              {listtwo && <li>{listtwo}</li>}
+              {listthree && <li>{listthree}</li>}
+            </ul>
           </div>
 
-          {/* BUTTON */}
-          <button className="btn skil-btn">
-            Apply Now
-          </button>
+          <div className="in-box">
+            <div className="in-wrap overflow-auto common-scrollbar">
+              <ul className="f-list">
+                {listfour && <li>{listfour}</li>}
+                {listfive && <li>{listfive}</li>}
+                {listsix && <li>{listsix}</li>}
+              </ul>
+            </div>
 
+            {/* Skills */}
+            <div>
+              <ul className="f-list skil-box">
+                {skilone && <li>{skilone}</li>}
+                {skiltwo && <li>{skiltwo}</li>}
+                {skilthree && <li>{skilthree}</li>}
+                {skilfour && <li>{skilfour}</li>}
+                {skilfive && <li>{skilfive}</li>}
+              </ul>
+
+              {/* APPLY BUTTON */}
+              <button type="button" className="btn inline-flex items-center justify-center skil-btn">
+                Apply Now
+                <svg xmlns="http://www.w3.org/2000/svg" width="9.071" height="14.844" viewBox="0 0 9.071 14.844">
+                  <path
+                    d="M13.995,13.058,8.222,7.285,9.871,5.636l7.422,7.422L9.871,20.48,8.222,18.831Z"
+                    transform="translate(-8.222 -5.636)"
+                  ></path>
+                </svg>
+              </button>
+            </div>
+          </div>
+
+          <div className="white-bg-feature"></div>
         </div>
       </div>
     </div>
